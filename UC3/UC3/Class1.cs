f2dfc23afcf6bcd169fb4d2c82c66226405fbf0b
unknown <buragohain4@gmail.com>
2022-08-22 17:04:38 +0530

    Adding UC3

---
+++ UC3/UC3/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace UC3
+{
+    class Class1
+    {
+        public int i;
+        public int Length;
+        public int L1;
+        public int L2;
+        public void CalculateLength()
+        {
+            for (i = 0; i < 2; i++)
+            {
+                Console.Write("Enter FIRST point coordinates:\n");
+                Console.Write("X Coordinate : ");
+                int x1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Y Coordinate : ");
+                int y1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter SECOND point coordinates:\n");
+                Console.Write("X Coordinate : ");
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Y Coordinate : ");
+                int y2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("We can represent the line by its 2 Points as: (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ")\n");
+                Length = Convert.ToInt32(Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2))));
+                if (i == 0)
+                {
+                    L1 = Length;
+                    Console.WriteLine("Length of the First Line is:" + L1 + "\n");
+                    Console.WriteLine("\n");
+                    Console.WriteLine("For Second Line:");
+                }
+                else
+                {
+                    L2 = Length;
+                    Console.WriteLine("Length of the Second Line is:" + L2 + "\n");
+                }
+            }
+        }
+        public void CompareLines()
+        {
+            if (L1 == L2)
+            {
+                Console.WriteLine("Lines are Equal.");
+            }
+            else if (L1 > L2)
+            {
+
+                Console.WriteLine("Line1 is longer than Line2.");
+            }
+            else
+            {
+                Console.WriteLine("Line1 is shorter than Line2.");
+            }
+        }
+    }
+}
---
+++ UC3/UC3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        public int L1;
+        public int L2;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! I am a big fan of Geometry :)");
+            Console.WriteLine("Let's play with Lines.");
+            Console.WriteLine("_---___------_____--------_______-----------_________--------------______________\n");
+            Console.WriteLine("Let us find length of any 2 lines.\n");
+            Console.WriteLine("For First Line:");
+            Class1 obj = new Class1();
+            obj.CalculateLength();
+            obj.CompareLines();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
