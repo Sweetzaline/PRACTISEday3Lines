b29b7d63cd369219492ab68c69ebce9082cb2e8c
unknown <buragohain4@gmail.com>
2022-08-22 17:02:11 +0530

    Adding UC1

---
+++ UC1/UC1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace UC2
+{
+    public class Class1
+    {
+        public int Length;
+        public void CalculateLength()
+        {
+            Console.Write("Enter FIRST point coordinates:\n");
+            Console.Write("X Coordinate : ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Y Coordinate : ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter SECOND point coordinates:\n");
+            Console.Write("X Coordinate : ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Y Coordinate : ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("We can represent the line by its 2 Points as: (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ")\n");
+
+            Length = Convert.ToInt32(Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2))));
+            Console.WriteLine("Length of the Line is:"+Length);
+        }
+    }
+}
---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! I am a big fan of Geometry :)");
+            Console.WriteLine("Let's play with Lines.");
+            Console.WriteLine("_---___------_____--------_______-----------_________--------------______________\n");
+            Console.WriteLine("Let us find length of any line.");
+            Class1 obj = new Class1();
+            obj.CalculateLength();
+        }
+    }
+}
